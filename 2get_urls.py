2f6d0d767fb15edee48bbdd45804d920ac916521
jaykwok <jay.kwok.xd@gmail.com>
2024-07-04 10:27:07 +0800

    update urls

--- 2get_urls.py
+++ 2get_urls.py
@@ -7,7 +7,7 @@ from playwright.async_api import async_playwright
 
 
 async def main():
-    url = 'https://cms.mylearning.cn/safe/topic/resource/2024/zxzq/pc.html'
+    url = 'https://cms.mylearning.cn/safe/topic/resource/2020/zhjtxtzq/pc.html'
     with open('./cookies.json', 'r') as f:
         cookies = json.load(f)
     async with async_playwright() as p:
