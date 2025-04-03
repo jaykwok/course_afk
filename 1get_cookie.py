a9839bc02c28002cb44f9328419ea9695b0619a3
jaykwok <jay.kwok.xd@gmail.com>
2025-04-03 15:51:12 +0800

    update url for getting cookies.

--- 1get_cookie.py
+++ 1get_cookie.py
@@ -8,7 +8,7 @@ def main():
         browser = p.chromium.launch(headless=False, channel="chrome")
         context = browser.new_context()
         page = context.new_page()
-        page.goto("https://cms.mylearning.cn/safe/topic/resource/2024/zxzq/pc.html")
+        page.goto("https://www.mylearning.cn/p5/index.html")
         frame = page.frame_locator("#esurfingloginiframe")
         auto_login = frame.locator("#j-auto-login-qr")
         # 检测是否勾选一周内自动登录，没有则勾上
@@ -16,9 +16,7 @@ def main():
             auto_login.click()
             print("已勾选一周内自动登录")
         print(f"是否一周内自动登录: {auto_login.is_checked()}")
-        page.wait_for_url(
-            "https://cms.mylearning.cn/safe/topic/resource/2024/zxzq/pc.html", timeout=0
-        )
+        page.wait_for_url("https://www.mylearning.cn/p5/index.html", timeout=0)
         with open("cookies.json", "w") as f:
             f.write(json.dumps(context.cookies()))
             print("已保存cookies")
