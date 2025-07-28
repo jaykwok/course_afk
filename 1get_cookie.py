25f03a549caac3d898f89964436333ca8c9a8970
jaykwok <jay.kwok.xd@gmail.com>
2025-07-28 16:39:32 +0800

    更新Cookie获取方式

--- 1get_cookie.py
+++ 1get_cookie.py
@@ -10,19 +10,27 @@ def main():
         browser = p.chromium.launch(headless=False, channel="msedge")
         context = browser.new_context()
         page = context.new_page()
-        page.goto("https://kc.zhixueyun.com/")
+        page.goto("https://www.mylearning.cn/p5/index.html")
+        page.wait_for_url("**/sso/login**", timeout=0)
 
-        # 检测是否勾选一周内自动登录, 没有则勾上
-        frame = page.locator("iframe").content_frame
-        auto_login = frame.locator("#j-auto-login-qr")
-        if not auto_login.is_checked():
-            auto_login.click()
-            print("已勾选一周内自动登录")
+        # 检测是否勾选30天内自动登录, 没有则勾上
+        # 获取iframe
+        iframe = page.locator("#esurfingloginiframe").content_frame
+        # 等待自动登录选项区域可见
+        iframe.locator("#j-auto-login-qr").wait_for()
+        # 点击下拉箭头展开选项列表
+        iframe.locator("#j-auto-group-qr .login-select").click()
+        # 等待选项列表出现并点击30天选项
+        iframe.locator("#j-auto-group-qr .login-option-list").wait_for()
+        iframe.locator(
+            '#j-auto-group-qr .login-option-list .option[data-time="3"]'
+        ).click()
+
+        iframe.locator("#j-auto-login-qr").click()
+        print("已勾选30天内自动登录")
 
         # 等待跳转到主页面
-        page.wait_for_url(
-            re.compile(r"https://kc\.zhixueyun\.com/#/home-v\?id=\d+"), timeout=0
-        )
+        page.wait_for_url("https://www.mylearning.cn/p5/index.html", timeout=0)
         with open("cookies.json", "w") as f:
             f.write(json.dumps(context.cookies()))
             print("已保存cookies")
